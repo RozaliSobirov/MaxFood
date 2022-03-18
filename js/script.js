613e1ec95953bc61bf5987999d27e6c929de4c6d
RozaliSobirov <99535278+RozaliSobirov@users.noreply.github.com>
2022-03-19 00:58:56 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function  and(){
+    document.getElementById("Navbar").style.display ="block"
+    document.getElementById("es").style.display = "none"
+}
+function  now(){
+    document.getElementById("Navbar").style.display ="none"
+    document.getElementById("es").style.display = "block"
+
+}
